69dd5aea965e1ac86915ecc7deb9086a5535faa9
Peter1577 <peterranadivemsp1577@gmail.com>
2023-04-23 00:50:36 +0530

    third commit

--- src/app/validators/passwordvalidation.ts
+++ src/app/validators/passwordvalidation.ts
@@ -1,22 +1,5 @@
-// import { AbstractControl } from "@angular/forms";
 
 import { AbstractControl } from "@angular/forms";
-
-// export function passwordMatch(Epwd:any ,Cpwd:any){
-
-//   return  function (form:AbstractControl){
-//         const passwordvalue = form .get(Epwd)?.value
-//         const Confirmpasswordvalue =form.get(Cpwd)?.value
-
-//         if(passwordvalue === Confirmpasswordvalue){
-//             return null;
-//         }
-
-//         return {PasswordMissMathcError:true}
-
-//     }
-// }
-
 
 export function PasswordMatch (Epwd:any , Password:any){
 
